972857a55ada64918da69ac1611044f47ac757d0
Alifur Rahman <alifurrahman1999@gmail.com>
2024-01-03 04:27:32 +0600

    update

--- modules/account/pagemenu/view.php
+++ modules/account/pagemenu/view.php
@@ -2,7 +2,7 @@
 // Module variables are available in page menus.
 // However, access group_id checking must be done directly from the page menu.
 // Minimal access checking such as $auth->actionAllowed('moduleName', 'actionName') should be performed.
-$groups  = AccountLevel::getArray();
+$groups = AccountLevel::getArray();
 
 $pageMenu = array();
 if ((AccountLevel::getGroupLevel($account->group_id) <= $session->account->group_level || $auth->allowedToEditHigherPower) && $auth->actionAllowed('account', 'edit')) {
